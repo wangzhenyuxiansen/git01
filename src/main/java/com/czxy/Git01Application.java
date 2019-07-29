223768ae38fd641a702717df4045495830a97226
wangzhenyuxiansen <wzy980524>
2019-07-29 15:53:57 +0800

    对对对

---
+++ src/main/java/com/czxy/Git01Application.java
@@ -0,0 +1,18 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git01Application.class, args);
+		/*System.out.println("传智小傻逼");
+		System.out.println("传智小傻逼");
+		System.out.println("传智小傻逼");
+		System.out.println("传智小傻逼");
+		System.out.println("传智小傻逼");*/
+	}
+
+}
