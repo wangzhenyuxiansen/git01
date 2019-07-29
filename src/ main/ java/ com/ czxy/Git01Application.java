b860a5462f235d4cdd0d0c50480c334e916ff3d5
王振宇 <wangzhenyuxiansen@gmail.com>
2019-07-29 15:56:32 +0800

    Update and rename src/main/java/com/czxy/Git01Application.java to  src/ main/ java/ com/ czxy/Git01Application.java

--- src/ main/ java/ com/ czxy/Git01Application.java
+++ src/ main/ java/ com/ czxy/Git01Application.java
@@ -8,7 +8,7 @@ public class Git01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Git01Application.class, args);
-		/*System.out.println("传智小傻逼");
+		/22222*System.out.println("传智小傻逼");2
 		System.out.println("传智小傻逼");
 		System.out.println("传智小傻逼");
 		System.out.println("传智小傻逼");
